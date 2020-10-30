02e1dece451b3a934c90145e8e5f4b59f7257b7d
Bob Holden <github@eawf.com>
2020-10-30 11:05:38 -0700

    Update getBTC.js

---
+++ Toolbox/getBTC.js
@@ -0,0 +1,4 @@
+# getBTC.js
+* Instead of re-creating the wheel, it's probably better to use:
+  - [bitcoinjs-lib](https://github.com/bitcoinjs/bitcoinjs-lib) - A javascript Bitcoin library for node.js and browsers.
+  - Kyle Honeycut's [buildl.js](https://github.com/coinables/buidljs) - Buidl.js is a wrapper for the bitcoinjs-lib library. 
